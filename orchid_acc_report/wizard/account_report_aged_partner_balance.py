66d62b1c3b305f8100a2b3a47fda3b6e8f38b3a9
Jamshid K <jamshid.kakunderi@betait.net>
2018-03-15 14:36:09 +0400

    xls report on aged partner balance

--- orchid_acc_report/wizard/account_report_aged_partner_balance.py
+++ orchid_acc_report/wizard/account_report_aged_partner_balance.py
@@ -50,7 +50,7 @@ class account_aged_trial_balance(osv.osv_memory):
                 
             result['data']['form']['used_context'] = used_context
         if context.get('xls_export'):
-            result['form']['partner_ids'] = data_toadd['od_partner_ids'] 
+            result['data'] = {'form':{'partner_ids':data_toadd['od_partner_ids'] + sales_person_ids}}
         return result
 
 
