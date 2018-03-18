9d9cb04e6fcaf36264c7ff2ede40ff2c1c31f512
Jamshid K <jamshid.kakunderi@betait.net>
2018-03-18 12:13:10 +0400

    inv schedule in anlaytic

--- orchid_beta_project/analytic.py
+++ orchid_beta_project/analytic.py
@@ -1137,7 +1137,7 @@ class od_project_invoice_schedule(models.Model):
             'account_id': account_id,
             'account_analytic_id': analytic_id ,
             'price_unit': line.price_unit or 0.0,
-            'quantity': line.quantity,
+            'quantity': line.product_uom_qty,
             'uos_id': line.uom_id.id or False,
             'product_id': line.product_id.id or False,
             'invoice_line_tax_id': [(6, 0, tax_id)],
