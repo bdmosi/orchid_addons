d38e681de2ce40af337a253eb0cc4298f64cea5d
Jamshid K <jamshid.kakunderi@betait.net>
2018-04-09 11:47:13 +0400

    onchange crm lead

--- orchid_cost_sheet/crm/crm.py
+++ orchid_cost_sheet/crm/crm.py
@@ -235,11 +235,11 @@ class crm_lead(osv.osv):
     def on_change_partner_id(self, cr, uid, ids, partner_id, context=None):
         values = super(crm_lead,self).on_change_partner_id(cr, uid, ids, partner_id, context=context)
         if partner_id:
-            sheet_pool= self.pool.get('od.cost.sheet')
-            sheet_ids = sheet_pool.search(cr,uid,[('lead_id','=',ids[0])])
-            print "sheets>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",sheet_ids,partner_id,type(partner_id)
-            for sheet in sheet_pool.browse(cr,uid,sheet_ids):
-                sheet.write({'od_customer_id':partner_id})
+#             sheet_pool= self.pool.get('od.cost.sheet')
+#             sheet_ids = sheet_pool.search(cr,uid,[('lead_id','=',ids[0])])
+#             print "sheets>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",sheet_ids,partner_id,type(partner_id)
+#             for sheet in sheet_pool.browse(cr,uid,sheet_ids):
+#                 sheet.write({'od_customer_id':partner_id})
             
             partner = self.pool.get('res.partner').browse(cr, uid, partner_id, context=context)
             values['value'].update( {
