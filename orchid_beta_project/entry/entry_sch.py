df26dc7dca391f2d2f5c139eb547b9aefac74d4d
Jamshid K <jamshid.kakunderi@betait.net>
2018-04-18 08:52:08 +0400

    entry check

--- orchid_beta_project/entry/entry_sch.py
+++ orchid_beta_project/entry/entry_sch.py
@@ -11,6 +11,8 @@ class entry_sch(models.Model):
     partner_ids = fields.Many2many('res.partner','entry_sch_part','en_sch_id','partner_id',string="Partner")
     move_id = fields.Many2one('account.move',string="Opening Entry")
     account_ids = fields.Many2many('account.account','entry_sch_accounts','en_sch_id','account_id',string="Accounts")
+    branch_id = fields.Many2one('od.cost.branch',string="Branch")
+    
     
     @api.multi
     def show_entries(self):
@@ -44,6 +46,23 @@ class entry_sch(models.Model):
                 'search_view_id': search_view and search_view[1] or False,
         }
         return value
+    @api.one 
+    def update_branch(self):
+        partner_ids = [pr.id for pr in self.partner_ids]
+        account_ids = [pr.id for pr in self.account_ids]
+        move_id  = self.move_id and self.move_id.id
+        branch_id = self.branch_id and self.branch_id.id 
+        domain =[]
+        if move_id:
+            domain +=[('move_id','=',move_id)]
+        if partner_ids:
+            domain +=[('partner_id','in',partner_ids)]
+        if account_ids:
+            domain +=[('account_id','in',account_ids)]
+            
+        entry_pool = self.env['account.move.line']
+        entry_ids =entry_pool.search(domain)
+        entry_ids.write({'od_branch_id':branch_id})
         
     
     
