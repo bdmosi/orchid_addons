75e96d3c60acdd18a26d96d4a0ff90c62712feaf
Jamshid K <jamshid.kakunderi@betait.net>
2018-05-07 09:50:10 +0400

    lead form parter branch write block

--- orchid_beta/crm/crm_lead.py
+++ orchid_beta/crm/crm_lead.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api,_
+from openerp import models, fields, api,
+from openerp.exception import Warning
 class crm_lead(models.Model):
     _inherit = "crm.lead"
     def get_saudi_company_id(self):
