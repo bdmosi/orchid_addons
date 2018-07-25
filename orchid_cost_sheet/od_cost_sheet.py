a7bbf68ab714f25f81f6fcc4ecb48e112060fb18
Jamshid K <jamshid.kakunderi@betait.net>
2018-07-25 15:34:44 +0400

    om mp

--- orchid_cost_sheet/od_cost_sheet.py
+++ orchid_cost_sheet/od_cost_sheet.py
@@ -497,7 +497,7 @@ class od_cost_sheet(models.Model):
         if self.included_om_in_quotation:
             vals = self.get_pdtgrp_vals()
             imp_vals = self.get_om_vals()
-            
+            print "pdt grop vals>>>>>>>>>>>>>>>>>>>>>>>>>",vals
             disc =0.0
     #         disc = abs(self.sp_disc_percentage)
             for imp_val in imp_vals:
