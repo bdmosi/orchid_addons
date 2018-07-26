027e495040eaae7ccdff93feca2f0f48c929a19c
Jamshid K <jamshid.kakunderi@betait.net>
2018-07-26 15:59:51 +0400

    beta proceduere seq

--- beta_procedures/models/beta_procedure.py
+++ beta_procedures/models/beta_procedure.py
@@ -6,6 +6,8 @@ from openerp.exceptions import Warning
 class Procedures(models.Model):
     
     _name = 'beta.procedures'
+    _description = "Beta Procedures"
+    _order = 'sequence'
     
     def od_get_company_id(self):
         return self.env.user.company_id
@@ -14,10 +16,13 @@ class Procedures(models.Model):
     image = fields.Binary(string='Attached Image')
     description = fields.Text()
     company_id = fields.Many2one('res.company', string='Company',default=od_get_company_id)
+    sequence = fields.Integer('sequence', help="Sequence for the handle.",default=10)
     
 class Structures(models.Model):
     
     _name = 'beta.structures'
+    _description = "Beta Structures"
+    _order = 'sequence'
     
     def od_get_company_id(self):
         return self.env.user.company_id
@@ -26,10 +31,13 @@ class Structures(models.Model):
     image = fields.Binary(string='Attached Image')
     description = fields.Text()
     company_id = fields.Many2one('res.company', string='Company',default=od_get_company_id)
-
+    sequence = fields.Integer('sequence', help="Sequence for the handle.",default=10)
+    
 class RoleDescription(models.Model):
     
     _name = 'role.description'
+    _description = "Role Description"
+    _order = 'sequence'
     
     def od_get_company_id(self):
         return self.env.user.company_id
@@ -38,10 +46,13 @@ class RoleDescription(models.Model):
     image = fields.Binary(string='Attached Image')
     description = fields.Text()
     company_id = fields.Many2one('res.company', string='Company',default=od_get_company_id)
+    sequence = fields.Integer('sequence', help="Sequence for the handle.",default=10)
     
 class BetaKPI(models.Model):
     
     _name = 'beta.kpi'
+    _description = "Beta KPIs"
+    _order = 'sequence'
     
     def od_get_company_id(self):
         return self.env.user.company_id
@@ -50,4 +61,5 @@ class BetaKPI(models.Model):
     image = fields.Binary(string='Attached Image')
     description = fields.Text()
     company_id = fields.Many2one('res.company', string='Company',default=od_get_company_id)
+    sequence = fields.Integer('sequence', help="Sequence for the handle.",default=10)
     
