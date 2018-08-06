5caec1b8ccec78a9f96b2bf05906ed7741dc8067
Jamshid K <jamshid.kakunderi@betait.net>
2018-08-06 16:41:24 +0400

    groups in hr job form

--- beta_customisation/hr/hr.py
+++ beta_customisation/hr/hr.py
@@ -129,10 +129,11 @@ class BetaJoiningForm(models.Model):
         groups_ids  =[group.id for group in groups]
         vals = {'name' : self.name,
                 'login' : self.work_email,
-                'groups_ids':[[6,False,groups_ids]]
+                'groups_id':[[6,False,groups_ids]]
             }
         default_vals.update(vals)
         user_id = user_pool.create(default_vals)
+#         user_id.write({'groups_id'})
         return user_id
     
     @api.one
